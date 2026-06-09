9daad10bc5becec6035966aeb14730ab537252cf
Rahul Tewari <47804389+lorddarcula@users.noreply.github.com>
2026-06-09 12:30:22 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 20
+float_type = 3.4
+bool_type = True
+string_type = "hello"
+list_type = [2,4]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
